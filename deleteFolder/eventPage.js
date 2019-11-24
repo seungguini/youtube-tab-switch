71de8e7b304687769f98437f8d56f621bd5b973e
seunggun <sl6369@nyu.edu>
2019-11-24 09:14:16 +0000

    changing authors....

--- deleteFolder/eventPage.js
+++
@@ -1,8 +0,0 @@
-// handle message from content.js and showPageAction
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-    if(request.todo == "showPageAction"){
-        chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
-            chrome.pageAction.show(tabs[0].id);
-        });
-    };
-});

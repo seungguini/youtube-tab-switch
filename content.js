ee5abdd5e56f40ab2d5ad92c489635926ce39115
seunggun <sl6369@nyu.edu>
2019-11-25 11:52:59 +0000

    log id of new Youtube Tabs

--- content.js
+++ content.js
@@ -1,13 +1,19 @@
+// this code will run each time a Youtube page is open
+
 // send message to eventPage when user watches a YouTube video
 // matching the URL format: "https://youtube.com/watch?v=*"
 chrome.runtime.sendMessage({todo: "showPageAction"});
+chrome.runtime.sendMessage({todo: "logVideoTab"});
+
+//chrome.storage.sync.set({'activeTabIDs': []})
+
+var vid = document.getElementsByClassName("video-stream html5-main-video");
 
 
 // listener for pausing video
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     if(request.todo == "pauseVideo"){
         // get(0) gets the native DOM element, which actually has the play() function
-        var vid = document.getElementsByClassName("video-stream html5-main-video");
         vid[0].pause();
     }
 });
@@ -16,7 +22,20 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     if(request.todo == "playVideo"){
         // get(0) gets the native DOM element, which actually has the play() function
-        var vid = document.getElementsByClassName("video-stream html5-main-video");
         vid[0].play();
+    }
+});
+
+// listener for pausing all sub-videos
+chrome.runtime.onMessage.addListener(function(request, sender, sendResposne){
+    if (request.todo == "pauseVideo"){
+        vid[0].pause();
+    }
+})
+
+$(function(){
+    // if the video is played, send message to pause all other videos
+    if (!video.paused) {
+        chrome
     }
 });
--- eventPage.js
+++ eventPage.js
@@ -1,3 +1,25 @@
+/*
+chrome.runtime.sendMessage({greeting:"hello"}, function(response) {
+    console.log(response.farewell);
+});
+
+chrome.tabs.query({active: true, currentWindow: true}, functions(tabs){
+    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response){
+        console.log(response.farewell());
+    });
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+    console.log(sender.tab ? "from a content script" + sender.tab.url: "from the extension");
+    if (request.greeting == "hello") P
+    sendResponse({farewell: "goodbye"});
+});
+
+// html video tag has property .paused
+*/
+
+var numTabs = 0;
+
 // handle message from content.js and showPageAction
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     if(request.todo == "showPageAction"){
@@ -6,3 +28,29 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
         });
     };
 });
+
+// logs tab id of a new YouTube tab
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+    if(request.todo == "logVideoTab"){
+        var tabArray;
+        chrome.tabs.getSelected(null, function(tab){
+            // if there are no current videos tabs playing, initialize array
+            if (numTabs == 0) {
+                chrome.storage.sync.get(tabArray:[], function(data){
+                    tabArray = data.tabArray;
+                    tabArray.push(tab.id);
+                });
+            }
+            // push tab id onto existing tabArray
+            else {
+                chrome.storage.sync.get('tabArray', function(data){
+                    tabArray = data.tabArray;
+                    tabArray.push(tab.id);
+                });
+            }
+            // set updated tabArray
+            chrome.storage.sync.set({'tabArray': tabArray});
+        });
+    };
+});
+

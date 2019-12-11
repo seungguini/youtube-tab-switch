20d5eb05d823e7442612b00e52367a30d561110d
seungguini <sl6369@nyu.edu>
2019-12-11 19:50:23 +0900

    toggle functionality works!

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,21 @@
+// set toggle value
+
+chrome.storage.sync.get({'enabled':true}, function(data){
+    if (data.enabled) {
+        $('#toggle-app').prop('checked', true);
+    } else {
+        $('#toggle-app').prop('checked', false);
+    }
+});
+
+
+// handle toggle
 $(function(){
-    $('.toggle-app').change(function() {
-        chrome.storage.sync.get({'enabled': true}, function(data){
-            var en = data.enabled;
-            en ^= en // toggle the boolean
-            chrome.storage.sync.set({'enabled': en});
-        });     
+    $('#toggle-app').change(function() {
+        if ($('#toggle-app').prop('checked')) {
+            chrome.storage.sync.set({'enabled': true});
+        } else {
+            chrome.storage.sync.set({'enabled': false});
+        }
     });
 });

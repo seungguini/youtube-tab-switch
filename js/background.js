a89a7f1cf3602ba8dde04d19bbcc50a4e383af2c
seungguini <sl6369@nyu.edu>
2019-12-18 12:35:31 +0900

    refresh all YouTube pages upon extension installation

--- js/background.js
+++ js/background.js
@@ -2,13 +2,45 @@
  * The background script. The main 'hub' for the extension, handling the TabArray which maintains all open YouTube tabs.
  * Communicates with each tab's content script to control video playback.
  */
-// log the tab id into chrome.storage
+
+/* Use activeTab to access the tab the user is currently on */
+
+/*
+When chrome installs and runs the extension for the first time, read and insert all YouTube tabs. Tabs are inserted
+from left to right, with the right-most tab having most priority
+*/
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.get({'tabArray':[]}, function(data){
+        chrome.tabs.query({'url': "*://*.youtube.com/watch?v=*"}, function (tabs) {
+            var tabArray = data.tabArray;
+            for (i in tabs) {
+                chrome.tabs.reload(tabs[i].id);
+                /*
+                tabArray = queueVideo(tabArray, tabs[i].id);
+
+                // inject script into each YouTube tab
+                chrome.tabs.executeScript(tabs[i].id, {file: "js/content.js"});
+                */
+            }
+
+            // play the right-most video
+            if (data.tabArray.length > 0) {
+                // when changed to enabled, play only the most recent vdieo
+                chrome.tabs.sendMessage(tabArray[tabArray.length-1], {todo: "playVideo"});
+            }
+
+            chrome.storage.sync.set({'tabArray': tabArray});
+        });
+    });
+});
+
+ // log the tab id into chrome.storage
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     // get number of Tabs active from chrome storage
     if(request.todo == "newVideo"){
-        var tabArray;
+        let tabArray;
         chrome.storage.sync.get({'tabArray':[]},function(data) {
-            var tabArray = data.tabArray;
+            let tabArray = data.tabArray;
             tabArray = queueVideo(tabArray, sender.tab.id);
 
             console.log(tabArray.toString())
@@ -23,14 +55,14 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     if (request.todo == "videoPlaying"){
         // get all current YouTube tabs
         chrome.storage.sync.get({'tabArray':[], 'enabled': true}, function(data){
-            var tabArray = data.tabArray;
+            let tabArray = data.tabArray;
             // reorder video play priority, so the this video is the last in array
             tabArray = reorderArray(tabArray, sender.tab.id);
 
             // pause all other videos ONLY if app is enabled
             if (data.enabled) {
                 // loop through all tabs
-                var index;
+                let index;
                 for (index in tabArray){
                     // exclude the current video
                     if (tabArray[index] != sender.tab.id) {
@@ -51,8 +83,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
    if (request.todo == "fillAudio"){
        chrome.storage.sync.get({'tabArray':[], 'enabled':true}, function(data){
           if(data.tabArray && data.enabled){
-            var tabArray = data.tabArray;
-            var length = tabArray.length;
+            let tabArray = data.tabArray;
+            let length = tabArray.length;
             // if the video paused is the main video
             if (sender.tab.id == tabArray[length-1]) {
                 if (length > 1) {
@@ -70,7 +102,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
    if (request.todo == "unqueueTab") {
         chrome.storage.sync.get('tabArray',function(data){
-            var tabArray = data.tabArray;
+            let tabArray = data.tabArray;
             // find the index of the id of the tab to remove in the tabArray
             // remove the id from tabArray
             spliceArray(tabArray,sender.tab.id);
@@ -85,7 +117,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 // @param tabArray the array to be spliced
 // @param id the id to be spliced
 function spliceArray(tabArray, id) {
-    var index = tabArray.indexOf(id);
+    let index = tabArray.indexOf(id);
     if (index > -1) {
         tabArray.splice(index, 1);
     }
@@ -96,7 +128,7 @@ function spliceArray(tabArray, id) {
 // @param the array to be reorganized
 // @param id the id to be reorganized
 function reorderArray(tabArray, id) {
-    var splicedArray = spliceArray(tabArray, id);
+    let splicedArray = spliceArray(tabArray, id);
     splicedArray.push(id);
     return splicedArray;
 }
@@ -107,17 +139,18 @@ function reorderArray(tabArray, id) {
 // @param tabArray the array that holds all video tab ids
 // @param id the tab to add to tabArray
 function queueVideo(tabArray, id) {
-    if (tabArray) {
+    var ta = tabArray;
+    if (ta) {
         // make sure tab is not duplicated
-        if (!tabArray.includes(id)) {
-            tabArray.push(id);
+        if (!ta.includes(id)) {
+            ta.push(id);
         } else { // if for some reason tab id already exists, bring it to the front
-            reorderArray(tabArray,id);
+            reorderArray(ta,id);
         }
     }
     else {
-        tabArray = [id];
+        ta = [id];
         //("tab initializing for the first time");
     }
-    return tabArray;
+    return ta;
 }
